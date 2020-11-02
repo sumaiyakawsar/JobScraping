3e91f404201601ae727d3fc59f6b9dce7d5abc4c
sumaiyakawsar <sumaiya.kawsar693@gmail.com>
2020-11-02 09:59:23 +0800

    LaTrobe Uploaded

---
+++ LaTrobeScrape/Postgraduate/PG_linkExtractor.py
@@ -0,0 +1,68 @@
+"""Description:
+    * author: Sumaiya Kawsar
+    * company: Fresh Futures/Seeka Technology
+    * position: IT Intern
+    * date: 30-10-20
+    * description:This script extracts all the courses links and save it in txt file.
+"""
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import os
+
+option = webdriver.ChromeOptions()
+option.add_argument(" - incognito")
+option.add_argument("headless")
+option.add_argument("start-maximized")
+exec_path = Path(os.getcwd().replace('\\', '/'))
+exec_path = exec_path.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
+browser = webdriver.Chrome(executable_path=exec_path, options=option)
+
+# MAIN ROUTINE
+courses_page_url = 'https://www.latrobe.edu.au/courses'
+list_of_groups = []
+list_of_links = []
+browser.get(courses_page_url)
+the_url = browser.page_source
+
+delay_ = 5  # seconds
+
+
+def click_btn():
+    browser.find_element_by_xpath("/html/body/div[1]/div/ul/li[2]/button").click()
+
+
+def all_faculty():
+    result_elements = browser.find_element_by_xpath('//*[@id="bodyContent"]/div[2]/div[3]/div/ol'). \
+        find_elements_by_tag_name('li')
+    for element in result_elements:
+        link = element.find_element_by_tag_name('a').get_property('href')
+        if "help" in link:
+            del link
+        else:
+            link = link + "/all?study_level=pg"
+            list_of_groups.append(link)
+
+
+# SAVE TO FILE
+def save_courses(link_list):
+    course_links_file_path = os.getcwd().replace('\\', '/') + '/LaTrobe_PG_links.txt'
+
+    course_links_file = open(course_links_file_path, 'w')
+    for link in link_list:
+        if link is not None and link != "" and link != "\n":
+            if link == link_list[-1]:
+                course_links_file.write(link.strip())
+            else:
+                course_links_file.write(link.strip() + '\n')
+    course_links_file.close()
+
+
+click_btn()
+all_faculty()
+save_courses(list_of_groups)
+
+browser.close()
---
+++ LaTrobeScrape/Undergraduate/DurationConverter.py
@@ -0,0 +1,65 @@
+import re
+
+
+def convert_num(text):  # okay... but why tho? what about zero?
+    return text.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five',
+                                                                                                           '5').replace(
+        'six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9')
+
+
+def convert_duration(duration):
+    duration = duration.lower()
+    duration = convert_num(duration)
+    numbers = re.findall(r'\d+(?:\.\d+)?', duration)
+    dur_type_list = []
+    for word in duration.split():
+        if 'semester' in word.lower() or 'term' in word.lower() or 'hour' in word.lower() or 'day' in word.lower() or 'week' in word.lower() or 'month' in word.lower() or 'year' in word.lower():
+            dur_type_list.append(word)  # put each word of the duration into the list
+
+    # put all numbers contained in the duration in this list
+    nums = []
+    for number in numbers:
+        if number != '':
+            nums.append(number)
+
+    for number in nums:
+        for dur in dur_type_list:
+
+            # CHECK FOR "YEAR" (But note that Year could also carry a month field in decimals!)
+            if 'year' in dur:
+                if '.' in number:
+                    if re.findall(r'(?:\d+\.)\d+', duration)[1] != 0:
+                        return convert_duration(str(round(float(number) * 12)) + ' month')
+                    return int(re.findall(r'(?:\d+\.)\d+', duration)[0]), 'Years'
+                else:
+                    return int(number), 'Years'
+
+            # CHECK FOR "MONTH"
+            elif 'month' in dur:
+                if '.' in number:
+                    if re.findall(r'(?:\d+\.)\d+', duration)[0] < 7:
+                        return convert_duration(str(int(float(number) * 4)) + ' week')
+                elif int(number) % 12 == 0:  # if the number is a sharp factor of 12
+                    return int(int(number) / 12), 'Years'
+                else:
+                    return int(round(float(number))), 'Months'  # if not, just round it up and return months
+
+            elif 'week' in dur:
+                return round(int(number)), ' Weeks'
+            elif 'hour' in dur:  # for real bruh??
+                return int(number), 'Hours'
+            elif 'semester' in dur:  # one semester being rounded to 6 months... sure.
+                return convert_duration(str(int(number) * 6) + 'month')
+            elif 'trimester' in dur:
+                return convert_duration(str(int(number) * 3) + 'month')
+            elif 'term' in dur:
+                return convert_duration(str(int(number) * 6) + 'month')
+            elif 'day' in dur:
+                if '.' in number:
+                    for jk in re.findall(r'\d+', duration):
+                        if int(jk) > 1:
+                            return convert_duration(str(int(float(number) * 24)) + 'hour')
+                else:
+                    return int(number), 'Days'
+            else:
+                return 'WRONG DATA'
