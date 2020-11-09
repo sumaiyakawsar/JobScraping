8051d450a4b4890cea051e446848af267a7d20a9
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2020-11-09 23:03:56 +0800

    Update UndergraduateListScraper.py

--- MonashScrape/Undergraduate/UndergraduateListScraper.py
+++ MonashScrape/Undergraduate/UndergraduateListScraper.py
@@ -4,34 +4,16 @@
     * company: Fresh Futures/Seeka Technologies
     * position: IT Intern
     * date: 20-10-20
-    * description:This program extracts the specific course links on each page of the given URL as specified by \n
-     \t CourseTypeLinkExtractor.py. The end results are fed to another program that tabulates the given data
+    * description:This program extracts the specific course links on each page of the given URL.
+     The end results are fed to another program that tabulates the given data
+
 """
 from pathlib import Path
 from selenium import webdriver
-import bs4 as bs4
-import requests
 import os
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import re
 
 
-def get_page(url):
-    try:
-        r = requests.get(url)
-        if r.status_code == 200:
-            return bs4.BeautifulSoup(r.content, 'html.parser')
-    except Exception as e:
-        pass
-    return None
-
-
-# selenium web driver
-# we need the Chrome driver to simulate JavaScript functionality
-# thus, we set the executable path and driver options arguments
-# ENSURE YOU CHANGE THE DIRECTORY AND EXE PATH IF NEEDED (UNLESS YOU'RE NOT USING WINDOWS!)
+# selenium web driver - extracts the links faster
 option = webdriver.ChromeOptions()
 option.add_argument(" - incognito")
 option.add_argument("headless")
@@ -39,34 +21,39 @@ exec_path = Path(os.getcwd().replace('\\', '/'))
 exec_path = exec_path.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
 browser = webdriver.Chrome(executable_path=exec_path, options=option)
 
-
 # MAIN ROUTINE
-course_type_links = []
-course_links = []
-each_url = 'https://www.monash.edu/study/courses/find-a-course?f.Tabs%7CcourseTab=Undergraduate&f.InterestAreas%7CcourseInterestAreas='
-browser.get(each_url)
-pure_url = each_url.strip()
-each_url = browser.page_source
+list_of_courses = []
+courses_page_url = 'https://www.monash.edu/study/courses/find-a-course?f.Tabs%7CcourseTab=Undergraduate&f.InterestAreas%7CcourseInterestAreas='
+browser.get(courses_page_url)
+the_url = browser.page_source
 
-soup = bs4.BeautifulSoup(each_url, 'html.parser')
 
-each_courses_links = soup.find_all('a', class_='box-featured__heading-link')
+def all_courses():
+    result_elements = browser.find_elements_by_css_selector("a.box-featured__heading-link")
+    for element in result_elements:
+        link = element.get_property('title')
+        link = link.replace("domestic", "international")
+        list_of_courses.append(link)
 
-for tag in each_courses_links:
-    var = tag['title'].replace("domestic", "international")
-    course_links.append(var)
+    print(len(list_of_courses))
+    print(list_of_courses)
 
-course_links_file_path = os.getcwd().replace('\\', '/') + '/monash_undergraduate_links.txt'
-course_links_file = open(course_links_file_path, 'w')
 
-print(len(each_courses_links))
+def save_courses(link_list, filename_):
+    course_links_file_path = os.getcwd().replace('\\', '/') + filename_
+    course_links_file = open(course_links_file_path, 'w')
 
-for i in course_links:
-    if i is not None and i is not "" and i is not "\n":
-        if i == course_links[-1]:
-            course_links_file.write(i.strip())
-        else:
-            course_links_file.write(i.strip()+'\n')
+    for link in link_list:
+        if link is not None and link != "" and link != "\n":
+            if link == link_list[-1]:
+                course_links_file.write(link.strip())
+            else:
+                course_links_file.write(link.strip() + '\n')
+    course_links_file.close()
 
-course_links_file.close()
-print(*course_links, sep='\n')
+
+all_courses()
+save_courses(list_of_courses, '/monash_undergraduate_links.txt')
+
+browser.quit()
+
