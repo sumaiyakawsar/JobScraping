46e418da7bd81708f6b7d84467c307197ce37c3a
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2020-12-01 13:00:24 +0800

    Update UNC_UG_ListExtractor.py

--- UNCScrap/Undergraduate/UNC_UG_ListExtractor.py
+++ UNCScrap/Undergraduate/UNC_UG_ListExtractor.py
@@ -10,18 +10,7 @@
 from pathlib import Path
 from selenium import webdriver
 import bs4 as bs4
-import requests
 import os
-
-
-def get_page(url):
-    try:
-        r = requests.get(url)
-        if r.status_code == 200:
-            return bs4.BeautifulSoup(r.content, 'html.parser')
-    except Exception as e:
-        pass
-    return None
 
 
 def clean_tags(soup_):
@@ -41,35 +30,48 @@ exec_path = exec_path.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe
 browser = webdriver.Chrome(executable_path=exec_path, options=option)
 
 # MAIN ROUTINE
-course_type_links = []
 course_links = []
-each_url = 'https://www.newcastle.edu.au/degrees#filter=level_undergraduate,level_undergraduate_honours,location_callaghan,location_newcastle_city,location_central_coast,location_sydney,location_online,type_single'
+each_url = 'https://www.newcastle.edu.au/degrees#filter=level_undergraduate,level_undergraduate_honours,' \
+           'location_callaghan,location_newcastle_city,location_central_coast,location_sydney,location_online,' \
+           'type_single '
 main_url = 'https://www.newcastle.edu.au/'
 browser.get(each_url)
 pure_url = each_url.strip()
 each_url = browser.page_source
 
-soup = bs4.BeautifulSoup(each_url, 'html.parser')
+soup = bs4.BeautifulSoup(each_url, 'lxml')
 clean_tags(soup)
 
-each_courses_links = soup.find_all('a', class_='degree-link')
-for tag in each_courses_links:
-    var = main_url + tag['href']
-    course_links.append(var)
 
-print(len(each_courses_links))
-
-course_links_file_path = os.getcwd().replace('\\', '/') + '/UNC_UG_links.txt'
-course_links_file = open(course_links_file_path, 'w')
-
-for i in course_links:
-    if i is not None and i is not "" and i is not "\n":
-        if i == course_links[-1]:
-            course_links_file.write(i.strip())
+def all_courses():
+    each_courses_links = soup.find_all('a', class_='degree-link')
+    for tag in each_courses_links:
+        var = main_url + tag['href']
+        if var not in course_links:
+            course_links.append(var)
         else:
-            course_links_file.write(i.strip()+'\n')
+            del var
 
-course_links_file.close()
-print(*course_links, sep='\n')
+    print(len(course_links))
+
+
+def save_courses(link_list, filename_):
+    course_links_file_path = os.getcwd().replace('\\', '/') + filename_
+    course_links_file = open(course_links_file_path, 'w')
+
+    for i in link_list:
+        if i is not None and i is not "" and i is not "\n":
+            if i == link_list[-1]:
+                course_links_file.write(i.strip())
+            else:
+                course_links_file.write(i.strip() + '\n')
+
+    course_links_file.close()
+    print(*course_links, sep='\n')
+
+
+all_courses()
+save_courses(course_links, '/UNC_UG_links.txt')
 
 browser.quit()
+
