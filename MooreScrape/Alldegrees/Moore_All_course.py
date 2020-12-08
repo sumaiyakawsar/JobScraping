9f1f22d3ce245796d10659e69749a9dbbe7eb63d
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2020-12-08 22:55:50 +0800

    Cleaned and Updated the scripts

---
+++ MooreScrape/Alldegrees/Moore_All_course.py
@@ -0,0 +1,221 @@
+"""Description:
+
+    * author: Sumaiya Kawsar
+    * company: Fresh Futures/Seeka Technologies
+    * position: IT Intern
+    * date: 25-11-20
+    * description:This program extracts the corresponding course details and tabulate it.
+"""
+import copy
+import pandas as pd
+import os
+import time
+from pathlib import Path
+
+import DurationConverter
+import TemplateData
+import bs4 as bs4
+from selenium import webdriver
+
+option = webdriver.ChromeOptions()
+option.add_argument("headless")
+option.add_argument('--no-sandbox')
+option.add_argument("start-maximized")
+option.add_argument("--disable-gpu")
+exec_path = Path(os.getcwd().replace('\\', '/'))
+exec_path = exec_path.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
+browser = webdriver.Chrome(executable_path=exec_path, options=option)
+
+# read the url from each file into a list
+course_links_file_path = Path(os.getcwd().replace('\\', '/'))
+course_links_file_path = course_links_file_path.__str__() + '/moore_links.txt'
+course_links_file = open(course_links_file_path, 'r')
+
+# the csv file we'll be saving the courses to
+csv_file_path = Path(os.getcwd().replace('\\', '/'))
+csv_file = csv_file_path.parent.__str__() + '/moore_allcourses.csv'
+
+number = r"(\d+,\d{3})*\.*\d*"
+currency_pattern = rf"\${number}"
+
+course_data_all = []
+
+level_key = TemplateData.level_key  # dictionary of course levels
+
+
+def tag_text(string_):
+    return string_.get_text().__str__().strip()
+
+
+def clean_tags(soup_):
+    for tag in soup_.find_all("span"):
+        tag.decompose()  # removes unecessary hidden text if called
+
+
+def has_numbers(input_string):
+    return any(char.isdigit() for char in input_string)
+
+
+def description(course_description):
+    for each_desc in course_description:
+        course_data['Description'] = each_desc.text.replace("\n", "").replace("About the course", "").strip()
+
+
+def durationo(p_word):
+    try:
+        if p_word:
+            if 'full time' in p_word.lower() or 'ft' in p_word.lower() and 'part time' not in p_word.lower():
+                course_data['Full_Time'] = 'Yes'
+            else:
+                course_data['Full_Time'] = 'No'
+
+            if 'part time' in p_word.lower() or 'part' in p_word.lower() and 'full time' not in p_word.lower() or 'ft' in p_word.lower():
+                course_data['Part_Time'] = 'Yes'
+            else:
+                course_data['Part_Time'] = 'No'
+
+            if 'year' in p_word.__str__().lower():
+                value_conv = DurationConverter.convert_duration(p_word)
+                duration = float(''.join(filter(str.isdigit, str(value_conv[0]))))
+                duration_time = value_conv[1]
+
+                if str(duration) == '1' or str(duration) == '1.0':
+                    course_data['Duration'] = duration
+                    course_data['Duration_Time'] = 'Year'
+                elif 'month' in duration_time.__str__().lower():
+                    course_data['Duration'] = duration
+                    course_data['Duration_Time'] = 'Months'
+
+                else:
+                    course_data['Duration'] = duration
+                    course_data['Duration_Time'] = duration_time
+
+            elif 'month' in p_word.__str__().lower():
+                value_conv = DurationConverter.convert_duration(p_word)
+                duration = float(''.join(filter(str.isdigit, str(value_conv[0]))))
+                duration_time = value_conv[1]
+                course_data['Duration'] = duration
+                course_data['Duration_Time'] = duration_time
+
+            elif 'week' in p_word.__str__().lower():
+                value_conv = DurationConverter.convert_duration(p_word)
+                duration = float(''.join(filter(str.isdigit, str(value_conv[0]))))
+                duration_time = value_conv[1]
+                course_data['Duration'] = duration
+                course_data['Duration_Time'] = duration_time
+            elif 'day' in p_word.__str__().lower():
+                value_conv = DurationConverter.convert_duration(p_word)
+                duration = float(''.join(filter(str.isdigit, str(value_conv[0]))))
+                course_data['Duration'] = duration
+                course_data['Duration_Time'] = 'Days'
+                if str(duration) == '1' or str(duration) == '1.0':
+                    course_data['Duration'] = duration
+                    course_data['Duration_Time'] = 'Day'
+            else:
+                course_data['Duration'] = ""
+                course_data['Duration_Time'] = ''
+        else:
+            course_data['Duration'] = ''
+            course_data['Duration_Time'] = ''
+
+    except Exception:
+        course_data['Duration'] = ''
+        course_data['Duration_Time'] = ''
+
+
+for each_url in course_links_file:
+    course_data = {'Level_Code': '', 'University': 'Moore Theological College', 'City': 'Sydney', 'Course': '',
+                   'Faculty': '', 'Int_Fees': '', 'Local_Fees': '', 'Currency': 'AUD', 'Currency_Time': 'Years',
+                   'Duration': '', 'Duration_Time': '', 'Full_Time': '', 'Part_Time': '',
+                   'Prerequisite_1': 'ATAR', 'Prerequisite_2': 'IELTS',
+                   'Prerequisite_1_grade_1': '', 'Prerequisite_2_grade_2': '',
+                   'Website': '', 'Course_Lang': 'English', 'Availability': 'A', 'Description': '',
+                   'Career_Outcomes/path': '', 'Country': 'Australia',
+                   'Online': '', 'Offline': 'Yes', 'Distance': '', 'Face_to_Face': '', 'Blended': '', 'Remarks': ''
+                   }
+    actual_cities = []
+    browser.get(each_url)
+    pure_url = each_url.strip()
+    each_url = browser.page_source
+
+    soup = bs4.BeautifulSoup(each_url, 'lxml')
+    time.sleep(1)
+
+    # Course-Website
+    course_data['Website'] = pure_url
+
+    # Course-name
+    course_name = soup.select("h1[itemprop='headline']")
+    if course_name:
+        for to in course_name:
+            course_title = tag_text(to)
+            course_data['Course'] = course_title
+    else:
+        course_data['Course'] = ""
+
+    # DECIDE THE LEVEL CODE
+    for i in level_key:
+        for j in level_key[i]:
+            if j in course_data['Course']:
+                course_data['Level_Code'] = i
+
+    # Duration
+    dura = soup.select("div.course-meta")
+    for to in dura:
+        p_word = to.text
+        durationo(p_word)
+
+    # Description
+    course_desc = soup.select("h2[itemprop='headline']")
+    if course_desc:
+        description(course_desc)
+    else:
+        course_data['Description'] = "N/A"
+
+    # IELTS/Local fees
+    if "ADIP" in course_data['Level_Code'] or "BA" in course_data['Level_Code']:
+        course_data['Prerequisite_2_grade_2'] = 7.0
+        course_data['Local_Fees'] = 21600
+        course_data['Online'] = "No"
+    elif "MST" in course_data['Level_Code'] or "PHD" in course_data['Level_Code']:
+        course_data['Prerequisite_2_grade_2'] = 7.5
+        course_data['Online'] = "No"
+    elif "DIP" in course_data['Level_Code']:
+        course_data['Prerequisite_2_grade_2'] = 6.5
+        course_data['Online'] = "Yes"
+    elif "CERT" in course_data['Level_Code']:
+        course_data['Online'] = "Yes"
+
+    if "Yes" in course_data['Online']:
+        course_data['Distance'] = "Yes"
+    else:
+        course_data['Distance'] = "No"
+
+    if "Yes" in course_data['Offline']:
+        course_data['Face_to_Face'] = "Yes"
+    else:
+        course_data['Face_to_Face'] = "No"
+
+    if "Yes" in course_data['Offline'] and "Yes" in course_data['Online']:
+        course_data['Blended'] = "Yes"
+    else:
+        course_data['Blended'] = "No"
+
+    print(course_data['Website'], course_data['Level_Code'], course_data['Online'])
+
+    course_data_all.append(copy.deepcopy(course_data))
+
+print(*course_data_all, sep='\n')
+
+desired_order_list = ['Level_Code', 'University', 'City', 'Course', 'Faculty',
+                      'Int_Fees', 'Local_Fees', 'Currency', 'Currency_Time',
+                      'Duration', 'Duration_Time', 'Full_Time', 'Part_Time',
+                      'Prerequisite_1', 'Prerequisite_2', 'Prerequisite_1_grade_1', 'Prerequisite_2_grade_2',
+                      'Website', 'Course_Lang', 'Availability', 'Description', 'Career_Outcomes/path', 'Country',
+                      'Online', 'Offline', 'Distance', 'Face_to_Face', 'Blended', 'Remarks']
+
+# tabulate our data
+df = pd.DataFrame(course_data_all, columns=desired_order_list)
+df.to_csv(csv_file, index=False)
+
+browser.quit()
